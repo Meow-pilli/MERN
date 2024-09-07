5c3f1d95a3fe3e7eb8b91ad5cb190f0badc9e968
Meow151 <meowpilli115@gmail.com>
2024-09-07 04:33:19 -0500

    api update

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,35 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const BudgetModel = require('./models/Total_Budget')
 //const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()
 
 //connect to mongobd
-mongoose.connect(process.env.MONGODB)
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({mssg: 'welcome to the app'})  //placeholder text
+})
+
+
+app.get("/getBudgets", (req, res) => {
+    BudgetModel.find()
+    .then(total_budgets => res.json(Misc))
+    .catch(err => res.json(err))
+});
+
+
+app.post("/postBudgets", async (req, res) => {
+    const user = req.body
+    const NewUser = new BudgetModel(user)
+    await NewUser.save()
+
+    res.json(user)
 })
 
 //routes
