25b5c8dc2ca7dcdeb85bcef49f571cfebb054a20
Meow151 <meowpilli115@gmail.com>
2024-09-11 05:17:55 -0500

    added Budget Calculator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
-function App() {
+function Users() {
   const [listOfUsers, setListOfUsers] = useState([]);
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
@@ -73,4 +73,103 @@ function App() {
   );
 }
 
-export default App;
+//Total Budget Calculator
+
+function Budget() {
+
+  // state
+  const [Gifts, setGifts] = useState(0)
+  const [Travel, setTravel] = useState(0)
+  const [FoodnDrinks, setFoodnDrinks] = useState(0)
+  const [Entertainment, setEntertainment] = useState(0)
+  const [Decorations, setDecorations] = useState(0)
+  const [CostumesnClothing, setCostumesnClothing] = useState(0)
+  const [StationarynPacking, setStationarynPacking] = useState(0)
+  const [CharitablenContributions, setCharitablenContributions] = useState(0)
+  const [Budget, setBudget] = useState('')
+  const [message, setMessage] = useState('')
+
+
+
+  let calcBudget = (event) => {
+    //prevent suBudgettting
+    event.preventDefault()
+
+    if (Gifts === 0 || Travel === 0) {
+      alert('Please enter a valid weight and Travel')
+    } else {
+      let Budget = (Number(Gifts) + Number(Travel) + Number(FoodnDrinks) + Number(Entertainment) + Number(Decorations)
+    + Number(CostumesnClothing)+ Number(StationarynPacking) + Number(CharitablenContributions))
+      setBudget(Budget)
+
+      // Logic for message
+
+      if (Budget < 500) {
+        setMessage('You are under Budget of 500')
+      } else {
+        setMessage('You are over Budget of 500')
+      }
+    }
+  }
+
+  //  show image based on Budget calculation
+
+
+  let reload = () => {
+    window.location.reload()
+  }
+
+  return (
+    <div className="app">
+      <div className='container'>
+        <h2 className='center'>Budget Calculator</h2>
+        <form onSubmit={calcBudget}>
+          <div>
+            <label>Gifts</label>
+            <input value={Gifts} onChange={(e) => setGifts(e.target.value)} />
+          </div>
+          <div>
+            <label>Travel</label>
+            <input value={Travel} onChange={(event) => setTravel(event.target.value)} />
+          </div>
+          <div>
+            <label>Food and Drinks</label>
+            <input value={FoodnDrinks} onChange={(e) => setFoodnDrinks(e.target.value)} />
+          </div>
+          <div>
+            <label>Entertainment</label>
+            <input value={Entertainment} onChange={(e) => setEntertainment(e.target.value)} />
+          </div>
+          <div>
+            <label>Decorations</label>
+            <input value={Decorations} onChange={(e) => setDecorations(e.target.value)} />
+          </div>
+          <div>
+            <label>Costumes Clothing</label>
+            <input value={CostumesnClothing} onChange={(e) => setCostumesnClothing(e.target.value)} />
+          </div>
+          <div>
+            <label>Stationary and Packing</label>
+            <input value={StationarynPacking} onChange={(e) => setStationarynPacking(e.target.value)} />
+          </div>
+          <div>
+            <label>Charitable Contributions</label>
+            <input value={CharitablenContributions} onChange={(e) => setCharitablenContributions(e.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+        </form>
+
+        <div className='center'>
+          <h3>Your Budget is: {Budget}</h3>
+          <p>{message}</p>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Budget;
